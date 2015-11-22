9b9f691e3d7ff17e04be724e4918b555d56a1e5b
Dean Putney <putney.dean@gmail.com>
2015-11-21 20:41:37 -0800

    Adding no-minimum tag for free albums, providing slightly more robust bandcamp site checking

--- bookmarklet.js
+++ bookmarklet.js
@@ -14,7 +14,7 @@ function getPostInfo() {
 }
 
 function getSite() {
-	if ('bandcamp' === $('meta[property="twitter:site"]').attr('content')) {
+	if (window.location.href.indexOf('bandcamp') > -1 || 'bandcamp' === $('meta[property="twitter:site"]').attr('content')) {
 		return 'bandcamp';
 	} 
 
@@ -28,8 +28,13 @@ function getBandcampInfo() {
 	album.artistLink = $('#name-section h3 span a').attr('href');
 	album.price = $('li.buyItem h4 .base-text-color').first().text().replace(' ', '') + 
 		' ' + $('li.buyItem h4 .buyItemExtra.secondaryText').first().text().replace(' ', '');
-	album.payWhatYouWant = album.price;
+	album.payWhatYouWant = false;
 	album.tags = [];
+	if (album.price.replace(/[\t\n ]/g, '').indexOf('nameyourprice') > -1) {
+		album.price = 0;
+		album.payWhatYouWant = true;
+		album.tags.push('no minimum');
+	}
 	var tags = $('.tralbum-tags a')
 	for (var i = 0; i < tags.length; i++) {
 		tag = $(tags[i]).text();
@@ -46,6 +51,8 @@ var tumblrPostURI = 'https://www.tumblr.com/widgets/share/tool?url=' + encodeURI
 	'&title=' + encodeURIComponent(album.title + ' by ' + album.artist) + 
 	'&caption=' + encodeURIComponent(' ') +
 	'&tags=' + encodeURIComponent(album.title + ',' + album.artist + ',' + album.tagString);
+
+console.log(tumblrPostURI);
 
 window.open(tumblrPostURI,'1448156228497','width=700,height=500,toolbar=0,menubar=0,location=0,status=0,scrollbars=0,resizable=1,left=0,top=0');
 
